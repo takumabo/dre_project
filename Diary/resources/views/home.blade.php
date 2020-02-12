7dfb91012f834e73efc2f91e01b2f51f73154399
takumabo <kingcongyatuhashi.1043@gmail.com>
2020-02-12 14:05:52 +0700

    home画面作成 home.blade.php home.js home.css 作成

--- Diary/resources/views/home.blade.php
+++ Diary/resources/views/home.blade.php
@@ -1,1 +1,60 @@
-hoge
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>ダナン不動産紹介アプリ</title>
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css">
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+</head>
+<body>
+    <!-- ドロワーメニュー -->
+    <div class="page">
+      <header tabindex="0">Da Nang Real Estate Service</header>
+          <div id="nav-container">
+            <div class="bg"></div>
+            <div class="button" tabindex="0">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </div>
+            <div id="nav-content" tabindex="0">
+              <ul>
+                <li><a href="#0">最新物件を見る</a></li>
+                <li><a href="#0">ロケーションで見る</a></li>
+                <li><a href="#0">物件価格で見る</a></li>
+                <li><a href="#0">ダナンフードロケーション</a></li>
+                <li><a href="#0">ダナン観光スポット</a></li>
+                <li class="small"><a href="#0">Facebook</a><a href="#0">Instagram</a></li>
+              </ul>
+            </div>
+        </div>
+    </div>
+    <!-- ドロワーメニュー -->
+    <div class="top-wrapper">
+        <div class="webcontent">
+            <a href="#">
+                <div class="font_awesome"><i class="far fa-building"></i></div>
+                <p>最新物件を見る</p>
+            </a>
+        </div>
+        <div class="webcontent">
+            <a href="#">
+                <div class="font_awesome"><i class="fas fa-map-marked-alt"></i></div>
+                <p>ロケーションで見る</p>
+            </a>
+        </div>
+        <div class="webcontent">
+            <a href="#">
+                <div class="font_awesome"><i class="fas fa-money-check-alt"></i></div>
+                <p>物件価格で見る</p>
+            </a>
+        </div>
+    </div>
+
+
+
+
+    <script src="js/home.js"></script>
+</body>
+</html>
