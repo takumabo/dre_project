81ba0805c1def577070fb665f445148d30501c3c
takumabo <kingcongyatuhashi.1043@gmail.com>
2020-02-14 21:36:53 +0700

    ロケーションで見るのページ作成完了。 googleMap 任意の場所に複数ピンで表示&ページ誘導完了。 #google map api #iframe 任意の場所に複数ピン表示 #ページ誘導 URLクリック 学び→layoutのみのjqueryCDNは全体のjsに浸透しない。 必ずjQueryを使用する際は、必ず各ページのscriptにjQueryCDNを書き込む。

--- Diary/resources/views/location.blade.php
+++ Diary/resources/views/location.blade.php
@@ -9,8 +9,13 @@
 </head>
 <body>
 @section('content')
-hoge
+<div class="location-map">
+   <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1kHpinHwIOdfEJwZUZTh4GlZPegpncr2D" width="640" height="480"></iframe>
+</div>
 @endsection
 
-<script src="js/location.js"></script>
+<footer>
+ <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+ <script src="js/location.js"></script>
+</footer>
 </body>
